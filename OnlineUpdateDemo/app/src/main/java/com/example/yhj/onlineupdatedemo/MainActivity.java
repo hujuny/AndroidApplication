351ca7e63ce9b13869583f3c533108ce6bf8e1f7
Stephen-hj <1364115532@qq.com>
2019-10-25 16:44:55 +0800

    在线升级Demo

--- OnlineUpdateDemo/app/src/main/java/com/example/yhj/onlineupdatedemo/MainActivity.java
+++ OnlineUpdateDemo/app/src/main/java/com/example/yhj/onlineupdatedemo/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main2);
 
         TextView tvVersion = findViewById(R.id.tv_version);
