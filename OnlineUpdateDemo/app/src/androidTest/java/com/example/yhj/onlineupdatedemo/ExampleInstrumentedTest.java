5a925f7e111cdf682c7cdfef5a1d436a1de5672a
Stephen-hj <1364115532@qq.com>
2019-10-28 15:34:05 +0800

    在线升级Demo

--- OnlineUpdateDemo/app/src/androidTest/java/com/example/yhj/onlineupdatedemo/ExampleInstrumentedTest.java
+++ OnlineUpdateDemo/app/src/androidTest/java/com/example/yhj/onlineupdatedemo/ExampleInstrumentedTest.java
@@ -4,10 +4,10 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
